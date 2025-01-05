7134247dca0c2fd19d4a4e1e6c1c90a8467682bf
RAMACHANDRAN A <125943533+rama563@users.noreply.github.com>
2025-01-05 21:03:22 +0530

    Time: 0 ms (100%), Space: 8.2 MB (6.34%) - LeetHub

---
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.c
@@ -0,0 +1,12 @@
+int numIdenticalPairs(int* nums, int n) {
+    int c=0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(nums[i] == nums[j])
+            c++;
+        }
+    }
+    return c;
+}
